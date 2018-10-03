c30ef9b0e8f5e66bd2f2b1e17f82a8d4ea852654
rahimbd7 <rahimbd7@gmail.com>
2018-10-03 18:34:36 +0600

    Project on number system

---
+++ project1.js
@@ -0,0 +1,8 @@
+let x = parseInt(process.argv[2]);
+let y = parseInt(process.argv[3]);
+
+console.log("Your Mathmatical Result are:");
+console.log('addition of '+x+' and '+y+' is:',x+y);
+console.log('subtraction from '+x+' to '+y+' is:',x-y);
+console.log('Multiplication of '+x+' and '+y+' is:',x*y);
+console.log('Division of '+x+' by '+y+' is:',x/y);
